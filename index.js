0e04f89e06eae619ed966a757ea56b1a3ffb33ec
AbigailSc <abigailsarzuri@gmail.com>
2022-09-06 15:56:23 -0300

    funca?

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const
-  app =
-    require('./src/app.js');
-const { conn } =
-  require('./src/db.js');
+const app = require('./src/app.js')
+const { conn } = require('./src/db.js')
 
-conn.sync({ force: true })
-  .then(() => {
-    app.listen(3001, () => {
-      console.log('%s listening at 3001') // eslint-disable-line no-console
-    })
+conn.sync({ force: true }).then(() => {
+  app.listen(3001, () => {
+    console.log('%s listening at 3001') // eslint-disable-line no-console
   })
+})
