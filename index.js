a1fafb3fe7970c4be956c539309d597c50ce129a
AbigailSc <abigailsarzuri@gmail.com>
2022-09-06 15:19:22 -0300

    chore: Husky implementation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('./src/app.js')
 const { conn } = require('./src/db.js')
 
-conn.sync({ force: true }).then(() => {
-  app.listen(3001, () => {
-    console.log('%s listening at 3001') // eslint-disable-line no-console
+conn.sync({ force: true })
+  .then(() => {
+    app.listen(3001, () => {
+      console.log('%s listening at 3001') // eslint-disable-line no-console
+    })
   })
-})
