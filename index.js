3ee16ede44ab49b2ca87959ad4970161e3287b2e
AbigailSc <abigailsarzuri@gmail.com>
2022-09-06 16:18:10 -0300

    asd

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const app = require("./src/app.js")
-const { conn } = require('./src/db.js')
-
+const app = require("./ src / app.js");
+const { conn } = require('./src/db.js');
 
 conn.sync({ force: true }).then(() => {
   app.listen(3001, () => {
